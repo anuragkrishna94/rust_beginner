301a378144ab0c8569d6800a9ee9224c0dbebb2f
Anurag Garimella <anurag.garimella94@gmail.com>
2023-05-21 22:35:23 +0530

    Chapter8 initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 mod chapters {
     pub mod chapter5;
     pub mod chapter6;
+    pub mod chapter8;
 }
 
 use crate::chapters::chapter5::chapter5_main;
 use crate::chapters::chapter6::chapter6_main;
+use crate::chapters::chapter8::chapter8_main;
 
 fn main() {
     println!("Hello, world!");
     chapter5_main();
     chapter6_main();
+    chapter8_main();
 }
