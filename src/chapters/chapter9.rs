74757c4d95f646656548d7611db13ea852d9e6d9
Anurag Garimella <anurag.garimella94@gmail.com>
2023-05-23 11:28:03 +0530

    Chapter 9

---
+++ src/chapters/chapter9.rs
@@ -0,0 +1,30 @@
+use std::fs::File;
+
+pub fn chapter9_main() {
+     // demo_file_simple_error(); // Uncomment to see how `panic!()` works
+     demo_file_create_file_on_error();
+}
+
+fn demo_file_simple_error() {
+     let hello_file_result: Result<File, std::io::Error> = File::open("hello.txt");
+     let hello_file: File:  = match hello_file_result {
+          Ok(file) => file,
+          Err(e) => panic!("Erred as : {:?}", E)
+     };
+}
+
+fn demo_file_create_file_on_error() { 
+     let hello_file_result: Result<File, std::io::Error> = File::open("hello.txt");
+     let hello_file: File = match hello_file_result {
+          Ok(file) => file,
+          Err(e) => match e.kind() {
+               std::io::ErrorKind::NotFound => match File::create("hello.txt") {
+                    Ok(file) => file,
+                    Err(e) => panic!("Unable to create new file: {:?}", e)
+               },
+               other_error => {
+                    panic!("Unable to understand: {:?}", other_error)
+               }
+          }
+     };
+}
--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,18 @@ mod chapters {
     pub mod chapter5;
     pub mod chapter6;
     pub mod chapter8;
+    pub mod chapter9;
 }
 
 use crate::chapters::chapter5::chapter5_main;
 use crate::chapters::chapter6::chapter6_main;
 use crate::chapters::chapter8::chapter8_main;
+use crate::chapters::chapter9::chapter9_main;
 
 fn main() {
     println!("Hello, world!");
     chapter5_main();
     chapter6_main();
     chapter8_main();
+    chapter9_main();
 }
