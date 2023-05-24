d893b4ec0c6fe22d8badf89fd52a29c1a6b67fa5
Anurag Garimella <anurag.garimella94@gmail.com>
2023-05-24 07:54:13 +0530

    Chapter 10 skeleton

---
+++ src/chapters/chapter10.rs
@@ -0,0 +1,3 @@
+pub fn chapter10_main() {
+     
+}
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ mod chapters {
     pub mod chapter6;
     pub mod chapter8;
     pub mod chapter9;
+    pub mod chapter10;
 }
 
 use crate::chapters::chapter5::chapter5_main;
 use crate::chapters::chapter6::chapter6_main;
 use crate::chapters::chapter8::chapter8_main;
 use crate::chapters::chapter9::chapter9_main;
+use crate::chapters::chapter10::chapter10_main;
 
 fn main() {
     println!("Hello, world!");
@@ -16,4 +18,5 @@ fn main() {
     chapter6_main();
     chapter8_main();
     chapter9_main();
+    chapter10_main();
 }
