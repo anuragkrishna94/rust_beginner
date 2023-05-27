6fd1df9ec34c83da3abb72844e80074275b80660
Anurag Garimella <anurag.garimella94@gmail.com>
2023-05-28 01:56:33 +0530

    Basics of Traits

--- src/chapters/chapter10.rs
+++ src/chapters/chapter10.rs
@@ -1,3 +1,40 @@
+struct NewsArticle {
+     headline: String,
+     location: String,
+     author: String,
+     content: String,
+     when: String
+}
+
+struct Tweet {
+     username: String,
+     content: String,
+     reply: bool,
+     retweet: bool,
+     when: String
+}
+
+trait Summary {
+     fn summarize(&self) -> String;
+}
+
+impl Summary for NewsArticle {
+     fn summarize(&self) -> String {
+         format!("News reads - {}, by: {}, at: {}, on: ({})", self.headline, self.author, self.location, self.when)
+     }
+}
+
+impl Summary for Tweet {
+     fn summarize(&self) -> String {
+         if self.retweet != true {
+          format!("Tweet, by: {}, on: {}", self.username, self.when)
+         }
+         else {
+          format!("ReTweet, by: {}, on: {}", self.username, self.when)
+         }
+     }
+}
+
 pub fn chapter10_main() {
      duplicate_largest_number();
      
@@ -14,6 +51,28 @@ pub fn chapter10_main() {
 
      let max_v2_2: &i32 = largest_item_module_generic(&v2);
      println!("Largest of {:?} is: {1}", v2, max_v2_2);
+
+     // `Traits` - Similar to interfaces in C# or Java
+     let news_1 = NewsArticle {
+          headline: String::from("Rust being picked by big tech companies"),
+          author: String::from("Anurag G"),
+          content: String::from("Rust's memory safety has give big tech companies an incentive to choose Rust over C/C++ and also their own proprietary languages"),
+          location: String::from("Hyderabad"),
+          when: String::from("20/05/2023")
+     };
+     let news1_summary = news_1.summarize();
+     println!("{}", news1_summary);
+
+     let tweet_1 = Tweet {
+          content: String::from("Rust seems to be taking the programming world by storm"),
+          username: String::from("anuragkrishna_g"),
+          reply: false,
+          retweet: false,
+          when: "28/05/2023".to_string()
+     };
+
+     let tweet1_summary = tweet_1.summarize();
+     println!("{}", tweet1_summary);
 }
 
 fn duplicate_largest_number() {
@@ -61,3 +120,6 @@ fn largest_item_module_generic<T: std::cmp::PartialOrd>(v: &[T]) -> &T {
      }
      max_item
 }
+
+// Monomorphization ensures that there is no performance overhead due to
+// the use of Generics in code.
