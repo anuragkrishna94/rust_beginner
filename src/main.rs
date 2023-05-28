435cce77fe856c3001a41b23fbf07a1117fd4436
Anurag Garimella <anurag.garimella94@gmail.com>
2023-05-28 15:53:52 +0530

    Chapter headers

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,18 @@ use crate::chapters::chapter10::chapter10_main;
 
 fn main() {
     println!("Hello, world!");
+    print_chapter_header(5);
     chapter5_main();
+    print_chapter_header(6);
     chapter6_main();
+    print_chapter_header(8);
     chapter8_main();
+    print_chapter_header(9);
     chapter9_main();
+    print_chapter_header(10);
     chapter10_main();
+}
+
+fn print_chapter_header(chapter_num: u8) {
+    println!("---------------------------START CHAPTER {}---------------------------", chapter_num);
 }
